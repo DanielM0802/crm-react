cc57fc103a1b80a2838b92eee32ea21607c64b07
Daniel <marillancadaniel@gmail.com>
2022-12-22 18:49:13 -0300

    navegacion entre componentes, estilos e iteracion de clientes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,21 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Link, useLocation } from "react-router-dom"
 
 function Layout() {
+
+  const {pathname} = useLocation();
+
   return (
-    <div>
-        <h1 className="text-6xl font-bold">CRM - React</h1>
-        <Outlet/>
+    <div className="md:flex md:min-h-screen">
+        <aside className="md:w-1/4 bg-emerald-900 px-5 py-10">
+          <h2 className="text-4xl font-black text-center text-white">CRM - Clientes</h2>
+          <nav className="mt-10">
+            <Link className={`${pathname === '/' ? 'text-emerald-300' :'text-white'} text-2xl block mt-2 hover:text-emerald-300`} to="/">Clientes</Link>
+            <Link className={`${pathname === '/clientes/nuevo' ? 'text-emerald-300' :'text-white'} text-2xl block mt-2 hover:text-emerald-300`} to="/clientes/nuevo">Nuevo Cliente</Link>
+          </nav>
+        </aside>
+        <main className="md:w-3/4 p-10 md:h-screen overflow-scroll">
+          <Outlet/>
+        </main>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,18 @@ import './index.css'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Layout from './components/Layout'
 import NuevoCliente from './pages/NuevoCliente'
+import Index, {loader as clientesLoader} from './pages/Index'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout/>,
     children: [
+      {
+        index: true,
+        element: <Index/>,
+        loader: clientesLoader
+      },
       {
         path: '/clientes/nuevo',
         element: <NuevoCliente/>
