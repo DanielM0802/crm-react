b608824f86eb731c489d495755bfcb1a87997d18
Daniel <marillancadaniel@gmail.com>
2022-12-22 19:58:34 -0300

    creando formulario de nuevo cliente

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom"
+import Formulario from "../components/Formulario";
 
 function NuevoCliente() {
+  //con -1 vuelve a la pag anterior
+  const navigate = useNavigate();
+
   return (
-    <div>NuevoCliente</div>
+    <>
+      <h1 className="font-black text-4xl text-emerald-900">Nuevo Cliente</h1>
+      <p className="mt-3">Completa todos los campos para registrar un nuevo cliente</p>
+
+      <div className="flex justify-end">
+          <button
+            className="bg-emerald-800 text-white px-3 py-1 font-bold uppercase"
+            onClick={() => navigate('/')}
+          >
+            Volver      
+          </button>
+      </div>
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20" >
+        <Formulario/>
+        <input 
+          type="submit" 
+          className="mt-5 w-full bg-emerald-800 p-3 uppercase font-bold text-white text-lg"
+          value="Registrar cliente"
+        />
+      </div>
+    </>
   )
 }
 
